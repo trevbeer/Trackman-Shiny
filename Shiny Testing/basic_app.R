6a81452f1d7b5638e2671695eb628d4f1c47ef4e
Taiyo Keilin <97199032+taiyokeilin@users.noreply.github.com>
2023-06-05 22:44:45 -0700

    Add files via upload
    some name and color changes

--- Shiny Testing/basic_app.R
+++ Shiny Testing/basic_app.R
@@ -17,8 +17,14 @@ game <- game %>% subset(TaggedPitchType != "Other")
 game <- game %>% subset(TaggedPitchType != "Knuckleball")
 # just ucla guys, not really necessary line
 game <- game %>% subset(PitcherTeam == "UCLA")
-game$Pitcher <- gsub("(.*),.*", "\\1", game$Pitcher)
+# game$Pitcher <- gsub("(.*),.*", "\\1", game$Pitcher)
+# game$Pitcher <- str_glue(str_split(game$Pitcher, ", ")[[1]][2], str_split(game$Pitcher, ", ")[[1]][1], .sep = " ")
+for (i in 1:nrow(game)) {
+  game$Pitcher[i] <- str_glue(strsplit(game$Pitcher[i], ', ')[[1]][2], strsplit(game$Pitcher[i], ', ')[[1]][1], .sep = ' ')
+}
 game$Date <- as.Date(game$Date, "%m/%d/%Y")
+pitch_colors <- c('Fastball' = '#d22d49', 'Sinker' = '#fe9d00', 'Cutter' = '#933f2c', 'Curveball' = '#00d1ed',
+                  'Slider' = '#c3bd0d', 'Sweeper' = '#CB9AF0', 'ChangeUp' = '#23be41', 'Splitter' = '#3bacac', 'Other' = '#Acafaf')
 
 ui <- fluidPage(
   
@@ -56,6 +62,7 @@ server <- function(input, output) {
     })
     ggplot(dataFilter(), aes(x = reorder(TaggedPitchType, -count), y = count, fill = TaggedPitchType)) + 
       geom_bar(stat = "identity") +
+      scale_fill_manual(values = pitch_colors) +
       labs(x = "Pitch Type", y = "Count", title = "Pitch Usage") +
       theme_bw() + theme(legend.position = "none", plot.title = element_text(hjust = 0.5, face = "bold", size = 16)) +
       theme(axis.title = element_text(size = 14), axis.text = element_text(size = 12))
@@ -70,6 +77,7 @@ server <- function(input, output) {
     })
     ggplot(dataFilter(), aes(x = reorder(TaggedPitchType, -RelSpeed), y = RelSpeed, fill = TaggedPitchType)) +
       geom_boxplot(width = 0.5) + 
+      scale_fill_manual(values = pitch_colors) +
       labs(x = "Pitch Type", y = "Velocity (MPH)", title = "Distribution of Pitch Velocity") +
       theme_bw() + theme(legend.position = "none", plot.title = element_text(hjust = 0.5, face = "bold", size = 16)) +
       theme(axis.title = element_text(size = 14), axis.text = element_text(size = 12))
